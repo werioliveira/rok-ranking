116e938ba5c0a0207bfa3904a3dc0cffca34475f
Weri Oliveira <werioliveira@hotmail.com>
2026-01-26 10:39:14 -0300

    adjust html lang to en-us

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="en-US">
       <body className={inter.className}>
         <Providers>
 
